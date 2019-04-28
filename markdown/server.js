05c033a0a20b50266a37c57bde60fd6984332b73
Rui Shu <terry.shu@hotmail.com>
2019-04-28 17:48:35 +0000

    update markdown server.js

--- markdown/server.js
+++ markdown/server.js
@@ -8,26 +8,14 @@ var args = process.argv.slice(2);
 var CMD  = args[0] || "test";
 
 var PORT = args.length == 2? args[1] : 9001;
-// var id = Math.floor(Math.random()*1000);
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
-
-app.get('/', function(req, res) 
-{
-	var sample_data = fs.readFileSync('index.html');
-	var text = marqdown.testing(sample_data);
-        res.send({preview: text});
-});
 
 app.post('/render', function(req, res) 
 {
-	// Use try block and return error
         var text = marqdown.render( req.body.markdown );
 	res.send({preview: text});
-	// res.send(req.body.markdown);
 });
 
 function start() 
